1ce237c46ed486048fb50d38827707d4057a1547
dohyung <dohk325@gmail.com>
2024-04-01 18:03:08 +0900

    Add Loading data part

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,2 +1,23 @@
 import os
+import pprint
 from os.path import join
+from pathlib import Path
+import copy
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+
+def display_info(obj: object, title: str = ""):
+    pprint.pp(" ##### " + title + " ##### ")
+    pprint.pp(obj)
+    print(end="\n\n")
+
+ROOT_PATH = os.getcwd()
+train_data_path = join(ROOT_PATH, '../data/', 'train.csv')
+
+train_data = pd.read_csv(train_data_path)
+
+display_info(train_data, "Train Data loaded")
+
