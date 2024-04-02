b1e95643955ebccb1fab23d564c70d9f2f8f4f5f
dohyung <dohk325@gmail.com>
2024-04-02 21:58:52 +0900

    Revise EDA

--- Utils.py
+++ Utils.py
@@ -9,19 +9,3 @@ class Utils:
         with open(file_path, 'r') as file:
             data = yaml.safe_load(file)
         return data
-
-    @staticmethod
-    def check_lib_version():
-        print("numpy version: {}".format(np.__version__))
-        print("pandas version: {}".format(pd.__version__))
-        print("matplotlib version: {}".format(matplotlib.__version__))
-        print("scikit-learn version: {}".format(sklearn.__version__))
-        print("xgboost version: {}".format(xgb.__version__))
-        print("lightgbm version: {}".format(lgb.__version__))
-        print("catboost version: {}".format(catboost.__version__))
-
-    @staticmethod
-    def display_info(obj: object, title: str = ""):
-        pprint.pp(" ##### " + title + " ##### ")
-        pprint.pp(obj)
-        print(end="\n\n")
