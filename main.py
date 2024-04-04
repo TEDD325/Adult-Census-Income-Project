7db4b0e148c1fd4224b147aca3d21f816ed4bafa
SuminHam <hsm4317@gmail.com>
2024-04-04 04:19:50 +0000

    model change

--- main.py
+++ main.py
@@ -1,86 +1,109 @@
-import preprocessing
 import os
 import warnings
-from scipy.stats import chi2_contingency
-import itertools
-import pprint
-import DataReader
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from imblearn.over_sampling import SMOTE
-import preprocessing
-from DataReader import *
-from DataUploader import *
-
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 
 from lightgbm import LGBMClassifier
-from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from DataReader import read_from_db
+from DataUploader import upload_to_db
+import preprocessing
+import optuna
+import multiprocessing
+
+n_cpus = multiprocessing.cpu_count()
+n_jobs = max(1, n_cpus - 1)
 
 def set_initial_setting():
     warnings.filterwarnings(action='ignore')
     plt.style.use("ggplot")
     pd.set_option('display.max_rows', 30)
     pd.set_option('display.max_columns', 500)
+    np.random.seed(42)
+    os.environ['PYTHONHASHSEED'] = str(42)
 
-    # seed 설정
-    seed_num = 42
-    np.random.seed(seed_num)
-    os.environ['PYTHONHASHSEED'] = str(seed_num)
+def preprocess_data(train_data, test, label, target_col):
+    train_data, test, label = preprocessing.run(train_data, test, label, target_col, verbose=0)
+    return train_data, test, label
 
+def save_submission_file(test_id, y_pred, submission_file_path):
+    submission_df = pd.DataFrame({'id': test_id, 'target': y_pred})
+    submission_df = submission_df.dropna().drop_duplicates(subset=['id'])
+    submission_df.to_csv(submission_file_path, index=False)
 
+def objective(trial):
+    # Define hyperparameters to be optimized
+    params = {
+        'num_leaves': trial.suggest_int('num_leaves', 20, 100),
+        'max_depth': trial.suggest_int('max_depth', -1, 15),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.003),
+        'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
+        'min_child_samples': trial.suggest_int('min_child_samples', 20, 100),
+        'min_child_weight': trial.suggest_loguniform('min_child_weight', 0.7, 1.0),
+        'subsample': trial.suggest_uniform('subsample', 0.7, 1.0),  # Bagging fraction
+        'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.7, 1.0),  # Feature fraction
+        'reg_alpha': trial.suggest_loguniform('reg_alpha', 0.0, 1.0),  # L1 regularization
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 0.0, 1.0),  # L2 regularization
+        'random_state': 42  # Fixed random state for reproducibility
+    }
+
+    # Initialize StratifiedKFold
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    # Initialize array to store average validation accuracy
+    avg_val_accuracy = []
+
+    # Perform cross validation
+    for train_index, test_index in skf.split(train_data, label):
+        x_train, x_valid = train_data.iloc[train_index], train_data.iloc[test_index]
+        y_train, y_valid = label[train_index], label[test_index]
+
+        # Train model with current hyperparameters
+        model = LGBMClassifier(**params, thread_count=n_jobs)
+        model.fit(x_train, y_train, verbose=False)
+
+        # Calculate validation accuracy
+        val_accuracy = model.score(x_valid, y_valid)
+        avg_val_accuracy.append(val_accuracy)
+
+    # Return average validation accuracy as objective value
+    return np.mean(avg_val_accuracy)
 
 if __name__ == "__main__":
     set_initial_setting()
 
     yaml_file_path = os.path.dirname(os.path.realpath(__file__)) + '/info.yaml'
     data_path = './data/'
-    target_col = 'target'
     table_name = "adult_income"
 
-    # ##### uploading data to PoestgreDB
-    # upload_to_db(yaml_file_path=yaml_file_path, data_path=data_path, table_name="adult_income")
+    # Read data from PoestgreDB
+    train_data, test, label = read_from_db(yaml_file_path=yaml_file_path, table_name=table_name, label_col_name='target')
+    test_id = test['id']
 
-    # ##### Read data from PoestgreDB
-    train_data, test, label = read_from_db(
-        yaml_file_path=yaml_file_path,
-        table_name=table_name,
-        label_col_name=target_col)
+    # Preprocess data
+    train_data, test, label = preprocess_data(train_data, test, label, 'target')
 
-    train_data, test, label = preprocessing.run(train_data, test, label, target_col, verbose=0)
+    # Define study object and optimize hyperparameters
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective, n_trials=10)
 
-    #train, valid 스플릿
-    x_train, x_valid, y_train, y_valid = train_test_split(train_data, label,
-                                                        test_size=0.3,
-                                                        random_state=42,
-                                                        shuffle=True,
-                                                        stratify=label)
+    # Get best hyperparameters
+    best_params = study.best_params
 
-    # 모델 학습
-    
-    # lgb boost
-    # lgb = LGBMClassifier()
-    # lgb.fit(x_train, y_train)
-    
-    # xgb = XGBClassifier(tree_method='hist')
-    # xgb.fit(x_train, y_train)
+    # Train final model with best hyperparameters
+    model = LGBMClassifier(**best_params, thread_count=n_jobs)
+    model.fit(train_data, label, verbose=False)
 
-    from sklearn.svm import SVC
-    svc = SVC()#(class_weight='balanced')
-    svc.fit(x_train, y_train)
+    # Make predictions
+    y_pred = model.predict(test)
 
-    # 예측
-    # predict=lgb.predict(test) #lgb boost
-    # predict=xgb.predict(test) #xg boost
-    predict = svc.predict(test)
-    # 제출 csv 생성
-    submission = pd.read_csv('sample_submission.csv') # sample_submission.csv는 프로젝트 폴더에 두었음
-    submission['target'] = predict
-    submission.to_csv('submit_svc.csv', index=False)
+    # Save submission file
+    submission_file = os.path.join(data_path, 'submission.csv')
+    save_submission_file(test_id, y_pred, submission_file)
 
-    # 전처리 결과를 다시 DB에 저장 후 불러온 후에 이어서 진행
+    # # Upload to DB
     # upload_to_db(
     #     yaml_file_path=yaml_file_path,
     #     table_name=f"preprocessed_{table_name}",
